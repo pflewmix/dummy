405d4f587251cf647efe3070534e1363ee84732e
pflewmix <119129701+pflemix@users.noreply.github.com>
2023-06-10 13:26:10 -0700

    First commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+//Adding
+function add7(a,b) {
+    return number + 7;
+}
+
+let number = parseInt
+(prompt("Input a number to solve the equation: x + 7 = ? "));
+
+alert("Your answer is: " + add7());
